15302139b8e04e78f4c039dbf0e5883d8696d461
Astro <astro@spaceboyz.net>
2010-08-29 08:03:48 +0200

    torrent_stream: reengineered using Arrays from outa space!

--- torrent_stream.js
+++ torrent_stream.js
@@ -11,127 +11,137 @@ function Stream(offset ,length) {
     // Where the HTTP stream is actually at
     this.offset = offset;
     this.length = length;
-    this.cache = {};  // desires by offset
+    this.cache = [];  // desires, ordered by offset
     this.growCache();
 }
 sys.inherits(Stream, EventEmitter);
 
 Stream.prototype.growCache = function() {
-    for(var o = this.offset;
+    var nextOffset = this.offset;
+    this.cache.forEach(function(desire) {
+			   if (nextOffset < desire.offset + desire.length)
+			       nextOffset = desire.offset + desire.length;
+		       });
+    for(var o = nextOffset;
         o < this.length && o < this.offset + CACHED_CHUNKS * CHUNK_SIZE;
         o += CHUNK_SIZE) {
 
-        if (!this.cache.hasOwnProperty(o))
-            this.cache[o] = { last: 0 };
+        this.cache.push({ offset: o, length: CHUNK_SIZE, last: 0 });
     }
+
+    /*console.log({grown:this.cache.map(function(desire) {
+					  return {
+					      offset: desire.offset,
+					      length: desire.length,
+					      last: desire.last
+					  };
+				      }),offset:this.offset});*/
 };
 
 Stream.prototype.nextDesired = function() {
     var now = Date.now();
-    var result;
-    for(var o in this.cache) {
-        if (this.cache.hasOwnProperty(o)) {
-            var desire = this.cache[o];
+    var best;
+    this.cache.forEach(function(desire) {
+			   if (desire.offset + desire.length <= this.offset) {
+			       // What's this doing here?
+			       console.log('Orphaned desire ' + desire.offset + '..' + desire.length + ', bad bad batshit');
+			       return;
+			   }
 
-	    if (desire.offset + desire.length <= o) {
-		// What's this doing here?
-		console.log('Eliminating orphaned desire ' + desire.offset + '..' + desire.length);
-		delete this.cache[o];
-		continue;
-	    }
+			   // request the same piece every 4s
+			   if (!desire.data && desire.last <= now - 4 * 1000) {
+			       // First things first
+			       if (!best || desire.offset < best.offset)
+				   best = desire;
+			   }
+		       });
 
-            // request the same piece every 4s
-            if (!desire.data && desire.last <= now - 4 * 1000) {
-                // First things first
-                if (!result || Number(o) < result.offset)
-                    result = { offset: Number(o),
-                               length: desire.length || CHUNK_SIZE };
-            }
-        }
-    }
-    if (result) {
-	var that = this;
-	result.requested = function() {
-	    console.log({requested:result.offset, offset:that.offset});
-            that.cache[result.offset].last = now;
+    if (best) {
+	return { offset: best.offset,
+		 length: best.length || CHUNK_SIZE,
+		 requested: function() {
+		     //console.log({next: best});
+		     best.last = Date.now();
+		 }
 	};
     }
-    return result;
 };
 
 Stream.prototype.receive = function(offset, data) {
-console.log({receive:offset,len:data.length});
+    //console.log({receive:offset,len:data.length});
 
-    if (this.cache.hasOwnProperty(offset)) {
-        var desire = this.cache[offset];
-	if (desire.data)
-	    console.log(offset + ' leeched twice');
-        desire.data = data.length <= CHUNK_SIZE ? data : data.slice(0, CHUNK_SIZE);
-
-        if (data.length < CHUNK_SIZE) {
+    var i, desire;
+    for(i = 0; i < this.cache.length; i++) {
+	desire = this.cache[i];
+	if (offset <= desire.offset && offset + data.length >= desire.offset) {
+	    data = data.slice(desire.offset - offset, data.length);
+	    if (desire.length < data.length) {
+		desire.data = data.slice(0, desire.length);
+		data = data.slice(desire.length, data.length);
+	    } else {
+		desire.data = data;
+		break;
+	    }
+	}
+    }
+    if (i < this.cache.length) {
+	if (desire.length > data.length) {
             // Too few data, create a smaller succeeding desire,
             // with last request set to now, so we can collect
             // succeeding buffers without re-requesting immediately.
-            this.cache[offset + data.length] = { last: Date.now(),
-                                                 length: CHUNK_SIZE - data.length };
-        } else if (data.length > CHUNK_SIZE) {
-            // Too many data, recurse for next chunk
-            // (wasn't requested, shouldn't happen)
-            this.receive(offset + CHUNK_SIZE, data.slice(CHUNK_SIZE, data.length));
-        }
+	    var newDesire = { offset: desire.offset + desire.data.length,
+			      length: desire.length - desire.data.length,
+			      last: Date.now() };
+	    this.cache.splice(i + 1, 0, newDesire);
+	    //console.log({inserted: newDesire});
+	}
+	desire.length = desire.data.length;
 
         this.walkCaches();
-    } else {
-	// Find a match
-	for(var o in this.cache)
-	    if (this.cache.hasOwnProperty(o)) {
-		var desire = this.cache[o];
-		if (offset < Number(o) && offset + data.length >= Number(o))
-		    this.receive(Number(o), data.slice(Number(o) - offset, data.length));
-	    }
-    }
+    } else
+	console.warn('Ouch: did not find cache desire for offset ' + offset);
 };
 
 // Resets cache entries
 Stream.prototype.cancelled = function(offset, length) {
+/* TODO:
+    this.cache.forEach()
+
+XXX:
     for(var o in this.cache)
         if (this.cache.hasOwnProperty(o)) {
             if (offset <= Number(o) && offset + length >= Number(o))
                 this.cache[o].last = 0;
         }
+*/
 };
 
 Stream.prototype.walkCaches = function() {
-    if (this.cache.hasOwnProperty(this.offset)) {
-        var desire = this.cache[this.offset];
-        if (desire.data) {
-            delete this.cache[this.offset];  // before write() advances offset
-            this.write(desire.data);
+    if (this.cache[0] && this.cache[0].data) {
+	var desire = this.cache.shift();
+	//console.log("shifted desire "+desire.offset+".."+desire.length);
+	if (this.offset !== desire.offset)
+	    console.error("ZOMFG! Cache got out of sync. We'll proceed sending garbage...");
+        this.write(desire.data);
 
-            this.growCache();
-
-            var that = this;
-            process.nextTick(function() {
-                                 that.walkCaches();
-                             });
-        }
-    } else
-	console.log('Inconsistency: no cache desire for current offset ' + this.offset);
-
+        this.growCache();
+        var that = this;
+        process.nextTick(function() {
+                             that.walkCaches();
+                         });
+    }
 
     // TODO: rm in production
     var s = '';
-    for(var o in this.cache) {
-        if (this.cache.hasOwnProperty(o))
-            s += this.cache[o].data ? '+' : '-';
-        else
-            s += '.';
-    }
+    this.cache.forEach(function(desire) {
+			   s += desire.data ? '+' : '-';
+		       });
     console.log(this.offset + ': ' + s);
 };
 
 Stream.prototype.write = function(data) {
     this.emit('data', data);
+    //console.log("write, offset = "+this.offset+" + "+data.length+" = "+(this.offset+data.length));
     this.offset += data.length;
     this.length -= data.length;
     if (this.length <= 0)
